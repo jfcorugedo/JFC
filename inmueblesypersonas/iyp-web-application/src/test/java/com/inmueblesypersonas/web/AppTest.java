0b8a93103354bcdeae3c828068405fd8d4a1808b
jfcorugedo <jfcorugedo@gmail.com>
2013-09-03 17:45:50 +0200

    se corrigen errores

---
+++ inmueblesypersonas/iyp-web-application/src/test/java/com/inmueblesypersonas/web/AppTest.java
@@ -0,0 +1,21 @@
+package com.inmueblesypersonas.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
